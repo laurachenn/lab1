496a96b347351424053ce2eafe3c4a7a6cbd6d14
laurachenn <37098012+laurachenn@users.noreply.github.com>
2018-03-16 13:49:39 +0800

    lab1_b

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+int n;
+cout << "enter an integer: ";
+cin >> n;
+
+while ( n != 1 ) 
+
+{
+
+cout << n << " ";
+
+if ( n % 2 == 1 )
+n = 3 * n + 1;
+
+else
+n = n/2;
+
+}
+
+cout << n << endl;
+
+return 0;
+
+}
+
